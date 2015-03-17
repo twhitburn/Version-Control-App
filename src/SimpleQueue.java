e96e0e61d926c913140b973547655fea290f489e
twhitburn <thomas@ElementaryOSWhitburn.(none)>
2015-03-16 22:34:25 -0500

    Added SimpleStack and SimpleQueue classes

---
+++ src/SimpleQueue.java
@@ -0,0 +1,34 @@
+
+public class SimpleQueue<E> implements QueueADT<E>  {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E dequeue() throws EmptyQueueException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enqueue(E item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E peek() throws EmptyQueueException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/SimpleStack.java
@@ -0,0 +1,34 @@
+
+public class SimpleStack<E> implements StackADT<E> {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E peek() throws EmptyStackException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E pop() throws EmptyStackException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(E item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
