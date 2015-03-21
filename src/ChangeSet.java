aed863ff82f119da18a2ff3e0c476b35f7d426e1
twhitburn <twhitburn@wisc.edu>
2015-03-21 12:46:45 -0500

    Implemented ChangeSet

--- src/ChangeSet.java
+++ src/ChangeSet.java
@@ -4,26 +4,24 @@
  *
  */
 public class ChangeSet {
-	
+
 	/* Queue of changes contained within the change set. */
 	private final  QueueADT<Change> changes;
-	
+
 	/* The name of the repository to which the changes belongs. */
 	private final String repoName;
-	
+
 	/**
 	 * Constructs a change set object. 
 	 * @param reponame The name of the repository.
 	 * @throws IllegalArgumentException if any argument is null.
 	 */
 	public ChangeSet(String repoName) {
-		// TODO: Implement this method. The following lines 
-		// are just meant for the method to compile. You can 
-		// remove or edit it in whatever way you like.
-		this.repoName = null;
-		this.changes = null;
+		
+		this.repoName = repoName;
+		this.changes = new SimpleQueue<Change>();
 	}
-	
+
 	/**
 	 * Adds (queues) a new change to the change set.
 	 * @param doc The doc to which the change was done.
@@ -31,9 +29,14 @@ public class ChangeSet {
 	 * @throws IllegalArgumentException if any argument is null.
 	 */
 	public void addChange(Document doc, Change.Type type) {
-		// TODO: Implement this method. 
+		if (doc == null || type == null) {
+			throw new IllegalArgumentException();
+		}
+		Change temp = new Change(doc, type);
+		changes.enqueue(temp);
+		
 	}
-	
+
 	/**
 	 * Returns the repository's name to which this change list belongs.
 	 * @return The repository's name.
@@ -41,29 +44,32 @@ public class ChangeSet {
 	public String getReponame() {
 		return this.repoName;
 	}
-	
+
 	/**
 	 * Returns and removes the next change from the change set.
 	 * @return The next change if present, null otherwise.
 	 */
 	public Change getNextChange() {
-		// TODO: Implement this method. The following lines 
-		// are just meant for the method to compile. You can 
-		// remove or edit it in whatever way you like.
-    	return null;
+
+		try {
+			return changes.dequeue();
+		} catch (EmptyQueueException e) {
+			System.out.println("No changes present.");
+		}
+		return null;
+
 	}
-	
+
 	/**
-	* Returns the count of changes contained in the change set.
-	* @return The count of changes.
-	*/
+	 * Returns the count of changes contained in the change set.
+	 * @return The count of changes.
+	 */
 	public int getChangeCount() {
-		// TODO: Implement this method. The following lines 
-		// are just meant for the method to compile. You can 
-		// remove or edit it in whatever way you like.
-    	return 0;
+		
+		return changes.size();
+		
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.changes.toString();	
